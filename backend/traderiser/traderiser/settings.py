8f036e514a41a596ac05ca821450481b830907d2
its-steo <steomustadd@gmail.com>
2025-11-14 19:04:49 +0300

    Add changes!

--- backend/traderiser/traderiser/settings.py
+++ backend/traderiser/traderiser/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #  SECURITY & ENVIRONMENT
 # ──────────────────────────────────────────────────────────────
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
